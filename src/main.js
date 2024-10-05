6669269a77f92350dc47b98f4e8298e7c691dfab
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2024-10-05 11:09:43 +0200

    Fixed and optimized main.js for dev environment

--- src/main.js
+++ src/main.js
@@ -9,26 +9,35 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'Home', component: () => import('./views/Home.vue') },
-        { path: '/about', name: 'About', component: () => import('./views/About.vue') },
+        // { path: '/about', name: 'About', component: () => import('./views/About.vue') },
         { path: '/contact', name: 'Contact', component: () => import('./views/Contact.vue') },
         { path: '/pricing', name: 'Pricing', component: () => import('./views/Pricing.vue') },
         { path: '/catalog', name: 'Catalog', component: () => import('./views/Catalog.vue') },
-        { path: '/booking', name: 'Booking', component: () => import('./views/Booking.vue') },
-
-        { path: '/:pathMatch(.*)*', redirect: { name: 'Home' } },
+        //{ path: '/booking', name: 'Booking', component: () => import('./views/Booking.vue') },
     ],
+});
+
+router.beforeEach((to, from, next) => {
+    if (!to.matched.length) {
+        next({ name: 'Home' });
+    } else {
+        document.title = 'Borealis | ' + to.name;
+        next();
+    }
 });
 
 const app = createApp(App);
 
 app.use(router);
 
-app.use(
-    createGtm({
-        id: "GTM-MNLKQ7WG",
-        vueRouter: router
-    })
-);
+if (process.env.NODE_ENV === 'production') {
+    app.use(
+        createGtm({
+            id: "GTM-MNLKQ7WG",
+            vueRouter: router
+        })
+    );
+}
 
 app.mount('#app')
 
