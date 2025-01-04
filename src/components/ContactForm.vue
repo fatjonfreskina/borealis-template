70fdae83e4e124a8e391e32dbeeabc9162845c0b
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2025-01-04 19:13:30 +0100

    Add function to validate the form
    Missing the actual submit action if the validation succeeds

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -7,15 +7,43 @@ const props = defineProps({
 
 const companyName = ref('Company Name');
 const companyQuote = ref('Company Quote');
+
+const checkValidity = function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  // TO DO: customize this
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+};
+
+const submitContactForm = function(){
+  const retVal = checkValidity();
+  console.log(retVal);
+  // TO DO: get the value of the validation
+}
 </script>
 
 <template>
   <div class="container mt-5">
-    <form>                
+    <form class="needs-validation" novalidate>
       <div class="col-md-6 mb-3">
         <label for="name" class="form-label">Name</label>
         <input type="text" class="form-control" id="name" placeholder="Your name" required />
         <div class="invalid-feedback">Please enter your name.</div>
+        <div class="valid-feedback"></div>
       </div>
       <div class="col-md-6 mb-3">
         <label for="email" class="form-label">Email address</label>
@@ -36,17 +64,7 @@ const companyQuote = ref('Company Quote');
         <input type="checkbox" class="form-check-input" id="sendCopy" />
         <label class="form-check-label" for="sendCopy">Send me a copy of this email</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button @click="submitContactForm" type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
-
-<style scoped>
-/* input {
-    max-width: 70%;
-}
-
-textarea {
-    max-width: 70%;
-} */
-</style>
